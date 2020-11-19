107298558ac06e9fa2bdf6cc3de0c5cc41a0b8a3
proyectoailnovo <clone1084@hotmail.com>
2020-11-19 03:21:05 -0400

    cambios sustanciales a compras, mejoras css, limpieza de código

--- Proyectofuncional/static/js/main.js
+++ Proyectofuncional/static/js/main.js
@@ -2,17 +2,55 @@
 function search_product() { 
     let input = document.getElementById('searchbar').value 
     input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('item'); 
-      
+    let x = document.getElementsByClassName('item');
+    let results=false;
+
     for (i = 0; i < x.length; i++) {  
         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
             x[i].style.display="none";
-            $(".mensajevacio").css("display","block");
+            
         } 
         else { 
-            x[i].style.display="list-item";
-            $(".mensajevacio").css("display","none");                  
+            x[i].style.display="block";
+            results=true;
+                           
         }
+    }
+    if (results){
+        $(".mensajevacio").css("display","none");   
     } 
+    else{
+        $(".mensajevacio").css("display","block");
+    }
 }
 /*Término del test*/ 
+function refreshcheckbox(){
+    numbeer=$('body').find(".item").length;
+
+    $('body').find(".item").each(function(index,element) {
+        console.log($(element).children());
+        if($(element).children()[1].checked==false){
+        numbeer--;
+        }   
+    });
+    if (numbeer==0) {
+        $("#buybtton").prop("disabled",true);
+    }
+    else{
+        $("#buybtton").prop("disabled",false);
+    }
+}
+function bruteforce(id_num){
+    verific=$("#num"+id_num).val();
+    if (verific=="") {
+        verific=1;
+    }
+    if(verific>30){
+        verific=30;
+    }
+    if(verific<0){
+        verific=1;
+    }
+    $("#num"+id_num).val(parseInt(verific));
+
+}
